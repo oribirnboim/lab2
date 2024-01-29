1796106d91e0c89a324dd44f0c066dd2de95e11d
oribirnboim <ori.birnboim@mail.huji.ac.il>
2024-01-29 10:26:50 +0200

    malus law for 2 polaraisers

--- polarization/malus.py
+++ polarization/malus.py
@@ -1,7 +1,9 @@
-import os
+from scipy .optimize import curve_fit
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import r2_score
+
 
 def get_data(filename):
     try:
@@ -26,17 +28,36 @@ def get_data(filename):
 
 
 
-def process_data(data):
-    return data
+def process_data(data, step):
+    x, y = data
+    x = np.arange(0, len(x)*step, step)
+    y = y * 10 ** 6
+    return x, y
 
 
 
 def malus2():
     data = get_data('malus2')
-    data = process_data(data)
-    x, y = data
-    plt.plot(x, y)
+    x, y = process_data(data, 10)
+    x_err = [2 for val in x]
+    y_err = [2 for val in y]
+    plt.errorbar(x, y, xerr=x_err, yerr=y_err, fmt='.', label='data')
+    def model(theta, i, d):
+        return i*np.cos(theta*np.pi/180 + d)**2
+    params, covariance = curve_fit(model, x, y, p0=[175, 0])
+    variance = np.diag(covariance)
+    print(params)
+    print(variance)
+    r2 = r2_score(y, model(x, *params))
+    print(r2)
+    x_fit = np.linspace(np.min(x), np.max(x), 1000)
+    y_fit = model(x_fit, *params)
+    plt.plot(x_fit, y_fit, label='fit')
+    plt.xlabel('degrees [$^\circ$]')
+    plt.ylabel('laser strength [$\mu$A]')
     plt.grid()
+    plt.legend()
+    plt.title('malus law for 2 polarisers')
     plt.show()
 
 
